24460509007ba01f238d04c06ff6f26b2aad68ba
Mario Prieto <mprietopereira@gmail.com>
2024-01-04 16:59:05 +0100

    First version of personal portfolio

---
+++ src/portfolio-data.ts
@@ -0,0 +1,30 @@
+export const PORTFOLIO_DATA = {
+  experience: [
+    {
+      company: 'ezzing',
+      date: 'Actualmente...',
+      description: 'Desarrollo de aplicaciones para la venta, gestión y monitorización de proyectos fotovoltaicos.',
+      title: 'Front-End Developer'
+    },
+    {
+      company: 'Vertiv',
+      date: 'Enero 2023',
+      description: 'Desarrollo de aplicación para monitorización de infraestructura crítica en tiempo real.',
+      title: 'Senior Software Engineer'
+    },
+    {
+      company: 'Sopra Steria',
+      date: 'Mayo 2018',
+      description: 'Desarrollo de aplicaciones web para clientes de España y Francia.',
+      title: 'Junior Programmer'
+    }
+  ],
+  projects: [
+    {
+      title: 'TodosJS',
+      header: 'Minimalist application to manage your daily tasks',
+      description: 'Creada desde cero con Vanilla JavaScript y webpack. Es una aplicación que permite gestionar tus tareas del día a día. Consta de linter, tests end-to-end y GitHub Actions.',
+      image: '/projects/todos-js.webp'
+    }
+  ]
+}
